7ee761ddb20eaa521d88b4c40a533c38afc7b9bf
Sushil <sushillikes@gmail.com>
2025-03-06 21:41:38 +0530

    Faculty data added

--- sql/ExamScheduler.session.sql
+++ sql/ExamScheduler.session.sql
@@ -106,6 +106,20 @@ INSERT INTO Student (student_id, first_name, last_name, email, phone_number, dep
 ('RA2311003010455', 'Gopal', 'Advani', 'gpladvn@gmail.com', '1234567898', 'Civil Engineering', 4, 8),
 ('RA2311003010459', 'Smruti', 'Parhi', 'smrprh  @gmail.com', '1234567899', 'Chemical Engineering', 5, 10);
 
+-- Insert data into Faculty table
+INSERT INTO Faculty (faculty_id, name, email, phone_number, department, availability_status) VALUES
+('RA2311003010FA001', 'Dr. D Viji', 'vijid@srmist.edu.in', '9876543210', 'Computer Science', TRUE),
+('RA2311003010FA002', 'Dr. Mahendran K', 'mahik@srmist.edu.in', '9876543211', 'Electrical Engineering', TRUE),
+('RA2311003010FA003', 'Dr. Sudestna Pathak', 'sudhp@srmist.edu.in', '9876543212', 'Mechanical Engineering', TRUE),
+('RA2311003010FA004', 'Dr. Sindhuja M', 'sidhum@srmist.edu.in', '9876543213', 'Civil Engineering', TRUE),
+('RA2311003010FA005', 'Dr. Ramaprabha J', 'ramuj@srmist.edu.in', '9876543214', 'Chemical Engineering', TRUE),
+('RA2311003010FA006', 'Dr. Vivek Justus', 'vivuj@srmist.edu.in', '9876543215', 'Computer Science', TRUE),
+('RA2311003010FA007', 'Dr. Deepa R', 'deepuR@srmist.edu.in', '9876543216', 'Electrical Engineering', TRUE),
+('RA2311003010FA008', 'Dr. Dhanavati G', 'dhanuG@srmist.edu.in', '9876543217', 'Mechanical Engineering', TRUE),
+('RA2311003010FA009', 'Dr. Senthil Kumar', 'sentK@srmist.edu.in', '9876543218', 'Civil Engineering', TRUE),
+('RA2311003010FA010', 'Dr. Geeta G ', 'gg@srmist.edu.in', '9876543219', 'Chemical Engineering', TRUE);
+
 -- Sample query to test the Student table
 SELECT * FROM Student;
+SELECT * FROM Faculty;
 
